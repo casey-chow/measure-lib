b8da12b37d9585e5815e4479c7d45b612e7f13ee
Casey Chow <casey.chow@gmail.com>
2017-12-06 14:07:40 -0500

    threading

--- ultrasonic_sensor.py
+++ ultrasonic_sensor.py
@@ -1,6 +1,7 @@
-# Adapted from https://tutorials-raspberrypi.com/raspberry-pi-ultrasonic-sensor-hc-sr04/
-import RPi.GPIO as GPIO
+# adapted from https://tutorials-raspberrypi.com/raspberry-pi-ultrasonic-sensor-hc-sr04/
+from threading import Thread
 import time
+import RPi.GPIO as GPIO
 
 SPEED_OF_SOUND = 13503.9 # inches per second
 
@@ -8,34 +9,49 @@ GPIO_TRIGGER = 17
 GPIO_ECHO = 27
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
 
-def distance(trigger, echo):
-    GPIO.output(trigger, True)
-    time.sleep(0.00001)
-    GPIO.output(trigger, False)
- 
-    while GPIO.input(echo) == 0:
-        pass
-    start_time = time.perf_counter()
+class DistanceMeasurementWorker(Thread):
+    def __init__(self, trigger, echo, poll_time, cb):
+        Thread.__init__(self)
+        self.trigger = trigger
+        self.echo = echo
+        self.poll_time = poll_time
+        self.cb = cb
 
-    while GPIO.input(echo) == 1:
-        pass
-    end_time = time.perf_counter()
+        GPIO.setup(self.trigger, GPIO.OUT)
+        GPIO.setup(self.echo, GPIO.IN)
 
-    elapsed = end_time - start_time
+    def run(self):
+        while True:
+            dist = self.distance()
+            self.cb(distance)
+            time.sleep(self.poll_time)
 
-    round_trip_distance = elapsed * SPEED_OF_SOUND
-    return round_trip_distance / 2
+    def distance(self):
+        GPIO.output(self.trigger, True)
+        time.sleep(0.00001)
+        GPIO.output(self.trigger, False)
 
+        start_time = time.perf_counter()
+        GPIO.wait_for_edge(self.echo, GPIO.FALLING)
+        end_time = time.perf_counter()
+
+        elapsed = end_time - start_time
+
+        round_trip_distance = elapsed * SPEED_OF_SOUND
+        return round_trip_distance / 2
 
 if __name__ == '__main__':
     try:
-        while True:
-            dist = distance(GPIO_TRIGGER, GPIO_ECHO)
-            print("distance: {}".format(dist))
-            time.sleep(1)
-    # reset by pressing CTRL + C
-    except KeyboardInterrupt:
+        print_distance = lambda distance: print("distance: {}".format(distance))
+
+        worker = DistanceMeasurementWorker(
+            trigger=GPIO_TRIGGER,
+            echo=GPIO_ECHO,
+            poll_time=1,
+            cb=print_distance,
+        )
+        worker.start()
+
+    finally:
         GPIO.cleanup()
