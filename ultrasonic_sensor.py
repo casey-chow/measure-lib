34033d27fac520d1f0b876564d4cb6252680bd58
Casey Chow <casey.chow@gmail.com>
2017-12-06 13:34:47 -0500

    reformat distance sensor and switch to inches

--- ultrasonic_sensor.py
+++ ultrasonic_sensor.py
@@ -2,52 +2,40 @@
 import RPi.GPIO as GPIO
 import time
 
+SPEED_OF_SOUND = 13503.9 # inches per second
+
 GPIO_TRIGGER = 17
 GPIO_ECHO = 27
 
 GPIO.setmode(GPIO.BCM)
-
-print('setup')
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
-def distance():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
- 
-    # set Trigger after 0.01ms to LOW
+def distance(trigger, echo):
+    GPIO.output(trigger, True)
     time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+    GPIO.output(trigger, False)
  
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
- 
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
- 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
- 
-    return distance
+    while GPIO.input(echo) == 0:
+        pass
+    start_time = time.perf_counter()
+
+    while GPIO.input(echo) == 1:
+        pass
+    end_time = time.perf_counter()
+
+    elapsed = end_time - start_time
+
+    round_trip_distance = elapsed * SPEED_OF_SOUND
+    return round_trip_distance / 2
 
 
 if __name__ == '__main__':
     try:
-        print('measure')
         while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
+            dist = distance(GPIO_TRIGGER, GPIO_ECHO)
+            print("distance: {}".format(dist))
             time.sleep(1)
- 
-        # Reset by pressing CTRL + C
+    # reset by pressing CTRL + C
     except KeyboardInterrupt:
-        print("Measurement stopped by User")
         GPIO.cleanup()
