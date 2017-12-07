41999bb80dff271956e61e29069d3edb64075639
Casey Chow <cc22@princeton.edu>
2017-12-07 19:48:29 +0000

    fixed the thresholding system

--- ultrasonic_sensor.py
+++ ultrasonic_sensor.py
@@ -1,18 +1,25 @@
 # adapted from https://tutorials-raspberrypi.com/raspberry-pi-ultrasonic-sensor-hc-sr04/
 import time
+import signal
+import sys
+import numpy as np
 import RPi.GPIO as GPIO
+
+from ring_buffer import RingBuffer
+
+DEBUG = True
 
 SPEED_OF_SOUND = 13503.9 # inches per second
 
-GPIO_TRIGGER = 17
-GPIO_ECHO = 27
+GPIO_TRIGGER = 4
+GPIO_ECHO = 17
 
+THRESHOLD = 40
 
 class UltrasonicSensor:
-    def __init__(self, trigger, echo, poll_time):
+    def __init__(self, trigger, echo):
         self.trigger = trigger
         self.echo = echo
-        self.poll_time = poll_time
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.trigger, GPIO.OUT)
@@ -20,11 +27,11 @@ class UltrasonicSensor:
 
     def distance(self):
         GPIO.output(self.trigger, True)
-        time.sleep(0.00001)
+        start_time = time.perf_counter()
+        time.sleep(0.0001)
         GPIO.output(self.trigger, False)
 
-        start_time = time.perf_counter()
-        GPIO.wait_for_edge(self.echo, GPIO.FALLING)
+        GPIO.wait_for_edge(self.echo, GPIO.FALLING, timeout=100)
         end_time = time.perf_counter()
 
         elapsed = end_time - start_time
@@ -32,16 +39,49 @@ class UltrasonicSensor:
         round_trip_distance = elapsed * SPEED_OF_SOUND
         return round_trip_distance / 2
 
+    def poll(self, poll_time, on_enter=None, on_exit=None):
+        print('starting polling')
+        history = RingBuffer(capacity=20, dtype=np.float)
+        entered = False
+        while True:
+            dist = self.distance()
+            history.append(dist)
+
+            if len(history) < 10:
+                continue
+
+            avg = np.median(history)
+            if DEBUG:
+                sys.stdout.write('\rdist: {} avg: {}'.format(dist, avg))
+
+            if not entered and avg < THRESHOLD:
+                entered = True
+                if on_enter:
+                    on_enter()
+            elif entered and avg > THRESHOLD:
+                entered = False
+                if on_exit:
+                    on_exit()
+            time.sleep(poll_time)
+
+
 if __name__ == '__main__':
-    try:
-        def print_distance(distance):
-            print("distance: {}".format(distance))
-
-        sensor = UltrasonicSensor(
-            trigger=GPIO_TRIGGER,
-            echo=GPIO_ECHO,
-            poll_time=1,
-        )
-
-    finally:
+    def signal_handler(sig, frame):
+        print()
         GPIO.cleanup()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    time.sleep(1)
+
+    sensor = UltrasonicSensor(
+        trigger=GPIO_TRIGGER,
+        echo=GPIO_ECHO,
+    )
+
+    sensor.poll(
+        poll_time=.01,
+        on_enter=lambda: print('entered!'),
+        on_exit=lambda: print('exited'),
+    )
